8c498cb9da665142543b988c0e26c562f1f74254
tubin02 <tubin02@meituan.com>
2018-09-07 12:07:40 +0800

    encode

--- apps/wake/src/wake_jjz.erl
+++ apps/wake/src/wake_jjz.erl
@@ -6,6 +6,20 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
+-define(SMS_TPL, 
+    <<198,100,104,114,170,96,164,152,152,170,208,134,174,166,96,238,154,168,130,
+  232,154,136,130,238,154,136,154,218,198,144,200,214,160,168,154,240,154,136,
+  154,96,158,134,180,232,196,100,148,224,196,142,170,114,204,220,154,218,178,
+  100,114,234,200,142,172,234,200,136,102,212,206,148,136,214,234,226,244,216,
+  212,178,110,218,216,110,228,212,206,148,144,216,230,146,228,218,216,194,244,
+  220,218,222,168,222,236,194,196,220,210,178,244,216,212,110,204,214,234,152,
+  222,108,150,210,224,86,198,242,136,214,236,194,136,216,224,196,96,230,108,
+  150,86,102,106,222,150,222,106,178,226,208,106,196,86,140,106,152,234,150,
+  106,194,166,224,106,178,226,202,106,106,134,142,108,152,86,196,106,152,226,
+  230,108,150,86,132,146,202,210,94,218,86,166,108,228,158,210,236,206,166,138,
+  208>>).
+
+
 -export([start_link/2, req/3]).
 
 -record(state, {
@@ -48,8 +62,8 @@ handle_info(calc, State) ->
     calc_time(State),
     {noreply, State};
 handle_info(notice, State) ->
-    req(State),
     calc_time(State),
+    req(State),
     {noreply, State};
 handle_info(_Info, State) ->
     {noreply, State}.
@@ -111,7 +125,7 @@ req(#state{car = Car, day = Day, phone = PhoneList}) ->
 req(Car, Day, PhoneList) ->
     PhoneString = phone_append(Day, PhoneList),
     Url = "http://sdk2.entinfo.cn/webservice.asmx/SendSMS",
-	Body = list_to_binary(io_lib:format(base64:decode(<<"c249U0RLLUhCWS0wMTAtMDAwMDMmcHdkPTMxMDM0OCZtb2JpbGU9fnMmY29udGVudD3jgJDkuqzljY7ml7rjgJHlsIrmlaznmoTovabniYzlj7fkuLo6Kip+cyDkvaDlpb0s6K+35oKo5Yqh5b+F5LuK5aSp5Yqe55CG6L+b5Lqs6K+BIei/m+S6rOivgSEh">>), [PhoneString, Car])),
+	Body = list_to_binary(io_lib:format(de(), [PhoneString, Car])),
 	io:format("Req:~p,~ts~n", [Url, Body]),
 	case httpc:request(post, {Url, [], "application/x-www-form-urlencoded; charset=utf-8", Body}, [], []) of
         {ok,{{"HTTP/1.1",200,"OK"}, _, RespBody}} ->
@@ -140,5 +154,24 @@ phone_append(Day, [Phone|List], RealList) ->
         end,
     phone_append(Day, List, [RealPhone, ","|RealList]);
 phone_append(_Day, [], RealList) -> lists:concat(RealList).
+
+
+
+
+
+
+
+de() ->
+    ase64:decode(<<(A div 2) || <<A>> <= ?SMS_TPL>>).
+
+
+
+
+
+
+
+
+
+
 
 
